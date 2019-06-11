6e283bab96452f6e33d9b096885d7cf81ab60e57
devansien <devansien@gmail.com>
2019-06-11 19:38:14 +1200

    batch update

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-'''
-#This example demonstrates the use of Convolution1D for text classification.
-
-Gets to 0.89 test accuracy after 2 epochs. </br>
-90s/epoch on Intel i5 2.4Ghz CPU. </br>
-10s/epoch on Tesla K40 GPU.
-'''
 from __future__ import print_function
 
 from keras.preprocessing import sequence
@@ -14,28 +7,26 @@ from keras.layers import Embedding
 from keras.layers import Conv1D, GlobalMaxPooling1D
 from keras.datasets import imdb
 
-# set parameters:
-max_features = 500
-maxlen = 400
-batch_size = 32
-embedding_dims = 5
-filters = 25
-kernel_size = 3
-hidden_dims = 250
-epochs = 2
+# # set parameters:
+# max_features = 500
+# maxlen = 400
+# batch_size = 32
+# embedding_dims = 5
+# filters = 25
+# kernel_size = 3
+# hidden_dims = 250
+# epochs = 2
 
-'''
-# More real parameters:
+
+# more real parameters:
 max_features = 5000
 maxlen = 400
-batch_size = 32
-embedding_dims = 50
-filters = 250
+batch_size = 64
+embedding_dims = 100
+filters = 500
 kernel_size = 3
 hidden_dims = 250
-epochs = 2
-'''
-
+epochs = 5
 
 print('Loading data...')
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
@@ -68,14 +59,14 @@ model.add(Conv1D(filters,
 # we use max pooling:
 model.add(GlobalMaxPooling1D())
 
-# We add a vanilla hidden layer:
+# we add a vanilla hidden layer:
 model.add(Dense(hidden_dims))
 model.add(Dropout(0.2))
 model.add(Activation('relu'))
 
-# We project onto a single unit output layer, and squash it with a sigmoid:
+# we project onto a single unit output layer, and squash it with a sigmoid:
 model.add(Dense(1))
-#model.add(Activation('sigmoid'))
+# model.add(Activation('sigmoid'))
 
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
